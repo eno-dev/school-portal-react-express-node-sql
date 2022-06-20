2e444c871dae4a3e90a6e43ac1566232f7a4e2e8
eno-sa <79256681+eno-sa@users.noreply.github.com>
2022-06-20 23:21:52 +0400

    cors fix

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
-    'http://localhost:3001'
+    'http://localhost:3001',
+    'school-portal-sern.herokuapp.com'
 ];
 
 module.exports = allowedOrigins;
