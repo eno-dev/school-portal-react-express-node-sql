a8e5a879111428504f5dc687bd90c7fc67b60a17
eno-sa <79256681+eno-sa@users.noreply.github.com>
2022-06-20 23:02:27 +0400

    commit after!

--- server/middleware/express.js
+++ server/middleware/express.js
@@ -9,7 +9,20 @@ const MySQLStore = require('express-mysql-session')(session);
 
 module.exports = app => {
 
+
     app.use(cors(corsOptions))
+    app.use(session({
+        secret: 'secret',
+        path: '/',
+        httpOnly: true,
+        secure: false,
+        cookie: {
+            sameSite: 'strict',
+            maxAge: 30000
+        },
+        resave: true,
+        saveUninitialized: true
+    }));
 
     // Json body parser
     app.use(bodyParser.json());
