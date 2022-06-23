b6817eed7366821692700fd5c9a90994e94d46a5
eno-sa <79256681+eno-sa@users.noreply.github.com>
2022-06-23 20:04:56 +0400

    commited!!!

--- client/src/components/sidebar-menu/sidebar-items.js
+++ client/src/components/sidebar-menu/sidebar-items.js
@@ -1,63 +1,126 @@
 import { NavLink } from "react-router-dom";
+import { useMediaQuery } from 'react-responsive'
 
 const SidebarItems = () => {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 810px)'
+    })
+    const isLargerThanMobile = useMediaQuery({ query: '(min-width: 480px)' })
+    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 480px)' })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
 
     return (
         <div className='menu-bar'>
-            <div className='menu'>
-                {/* <!-- All the menu links --> */}
-                <ul className="menu-links">
-                    <li className="nav-link">
-                        <NavLink to="/">
-                            <i className='bx bxs-dashboard icon' title="Dashboard"></i>
-                            <span className="text nav-text">Dashboard</span>
-                        </NavLink>
-                    </li>
+            {isTabletOrMobile &&
+                <div className='menu'>
+                    {/* <!-- All the menu links --> */}
+                    <ul className="menu-links">
+                        <li className="nav-link">
+                            <NavLink to="/">
+                                <i className='bx bxs-dashboard icon' title="Dashboard"></i>
+                                <span className="text nav-text">Dashboard</span>
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink to="/database">
+                                <i className='bx bx-data icon' title="Database"></i>
+                                <span className="text nav-text">Database</span>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav-link">
-                        <NavLink to="/database">
-                            <i className='bx bx-data icon' title="Database"></i>
-                            <span className="text nav-text">Database</span>
-                        </NavLink>
-                    </li>
+                        <li className="nav-link">
+                            <NavLink to='/grades'>
+                                <i className='bx bx-bar-chart-alt-2 icon' title="Grades"></i>
+                                <span className="text nav-text">Grades</span>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav-link">
-                        <NavLink to='/grades'>
-                            <i className='bx bx-bar-chart-alt-2 icon' title="Grades"></i>
-                            <span className="text nav-text">Grades</span>
-                        </NavLink>
-                    </li>
+                        <li className="nav-link">
+                            <NavLink to="/search">
+                                <i className='bx bx-search icon' title="Search"></i>
+                                <span className="text nav-text">Search</span>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav-link">
-                        <NavLink to="/search">
-                            <i className='bx bx-search icon' title="Search"></i>
-                            <span className="text nav-text">Search</span>
-                        </NavLink>
-                    </li>
+                        <li className="nav-link">
+                            <NavLink to="/notifications">
+                                <i className='bx bx-bell icon' title="Notifications"></i>
+                                <span className="text nav-text">Notifications</span>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav-link">
-                        <NavLink to="/notifications">
-                            <i className='bx bx-bell icon' title="Notifications"></i>
-                            <span className="text nav-text">Notifications</span>
-                        </NavLink>
-                    </li>
+                        <li className="nav-link">
+                            <NavLink to="/analytics">
+                                <i className='bx bx-pie-chart-alt icon' title="Analytics"></i>
+                                <span className="text nav-text">Analytics</span>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav-link">
-                        <NavLink to="/analytics">
-                            <i className='bx bx-pie-chart-alt icon' title="Analytics"></i>
-                            <span className="text nav-text">Analytics</span>
-                        </NavLink>
-                    </li>
+                        <li className="nav-link">
+                            <NavLink to="/attendance">
+                                <i className='bx bx-heart icon' title="Attendance"></i>
+                                <span className="text nav-text">Attendance</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>}
 
-                    <li className="nav-link">
-                        <NavLink to="/attendance">
-                            <i className='bx bx-heart icon' title="Attendance"></i>
-                            <span className="text nav-text">Attendance</span>
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-            {/* <Outlet /> */}
+            {isLargerThanMobile &&
+                <div className='menu'>
+                    <ul className="menu-links">
+                        <li className="nav-link">
+                            <NavLink to="/">
+                                <i className='bx bxs-dashboard icon' title="Dashboard"></i>
+                                <span className="text nav-text">Dashboard</span>
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink to="/database">
+                                <i className='bx bx-data icon' title="Database"></i>
+                                <span className="text nav-text">Database</span>
+                            </NavLink>
+                        </li>
+
+                        <li className="nav-link">
+                            <NavLink to='/grades'>
+                                <i className='bx bx-bar-chart-alt-2 icon' title="Grades"></i>
+                                <span className="text nav-text">Grades</span>
+                            </NavLink>
+                        </li>
+
+                        <li className="nav-link">
+                            <NavLink to="/search">
+                                <i className='bx bx-search icon' title="Search"></i>
+                                <span className="text nav-text">Search</span>
+                            </NavLink>
+                        </li>
+
+                        <li className="nav-link">
+                            <NavLink to="/notifications">
+                                <i className='bx bx-bell icon' title="Notifications"></i>
+                                <span className="text nav-text">Notifications</span>
+                            </NavLink>
+                        </li>
+
+                        <li className="nav-link">
+                            <NavLink to="/analytics">
+                                <i className='bx bx-pie-chart-alt icon' title="Analytics"></i>
+                                <span className="text nav-text">Analytics</span>
+                            </NavLink>
+                        </li>
+
+                        <li className="nav-link">
+                            <NavLink to="/attendance">
+                                <i className='bx bx-heart icon' title="Attendance"></i>
+                                <span className="text nav-text">Attendance</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            }
         </div >
     )
 }
