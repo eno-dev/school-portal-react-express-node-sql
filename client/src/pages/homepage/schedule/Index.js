b78ab6403b59032ae0653dd075c48be79f730f2c
eno-sa <79256681+eno-sa@users.noreply.github.com>
2022-07-01 09:03:24 +0400

    pdf test

--- client/src/pages/homepage/schedule/Index.js
+++ client/src/pages/homepage/schedule/Index.js
@@ -1,9 +1,17 @@
+import myPDF from '/Users/enosaliu/Downloads/StudentsT3.pdf'
 const Index = () => {
     return (
         <div>
             <h1>
                 Schedule
             </h1>
+            <iframe
+                src={myPDF}
+                frameBorder="0"
+                scrolling="auto"
+                height="100%"
+                width="100%"
+            ></iframe>
         </div>
     )
 }
